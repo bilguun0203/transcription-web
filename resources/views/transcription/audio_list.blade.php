deb7740584747f86b85efe6b630d1027dbfb20b4
Bilguun Ochirbat <bilguun0203@gmail.com>
2017-12-22 23:26:45 +0800

    Аудио файлуудын жагсаалт

--- resources/views/transcription/audio_list.blade.php
+++ resources/views/transcription/audio_list.blade.php
@@ -3,7 +3,6 @@
 @section('title', 'Prototype - Validation')
 
 @section('additional_stylesheet')
-    <link rel="stylesheet" href="{{ asset('assets/css/dataTables.bootstrap4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/plyr.css') }}">
 @endsection
 
@@ -11,16 +10,37 @@
     <div class="container" style="margin-top: 20px;">
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
-                <!--<div class="row justify-content-center">-->
-                <!--<div class="col-lg-12 col-md-8 col-sm-10">-->
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Файлууд /0 - 100/</h4>
-                        <!--<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>-->
-                        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
-                        <!--<a href="#" class="card-link">Card link</a>-->
-                        <!--<a href="#" class="card-link">Another link</a>-->
-
+                        <h4 class="card-title">Файлууд</h4>
+                        <h6 class="card-subtitle text-muted mt-2"><strong>Нийт:</strong> {{ $total_rows }} <strong>Хүснэгтэнд:</strong> {{ $row_from }} - {{ $row_to }}</h6>
+                        <hr>
+                        @foreach($errors->all() as $error)
+                            <div class="row justify-content-center">
+                                <div class="col-md-6 col-sm-8 col-xs-12">
+                                    <div class="alert alert-danger">
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true"><i class="fal fa-times"></i></span>
+                                            <span class="sr-only">Хаах</span>
+                                        </button>
+                                        {{ $error }}
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                        @if($results == 0)
+                            <div class="row justify-content-center">
+                                <div class="col-md-6 col-sm-8 col-xs-12">
+                                    <div class="alert-danger">
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true"><i class="fal fa-times"></i></span>
+                                            <span class="sr-only">Хаах</span>
+                                        </button>
+                                        Үр дүн олдсонгүй
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
                         <div class="table-responsive">
                             <table class="table" id="audio-table">
                                 <thead>
@@ -28,88 +48,105 @@
                                     <th style="width: 1%;">#</th>
                                     <th style="width: 25%;">Аудио</th>
                                     <th style="width: 50%;">Бичвэр</th>
-                                    <th>Төлөв</th>
-                                    <th style="width: 15%;">Үйлдэл</th>
+                                    <th style="width: 1%;">Үлдсэн шалгалт</th>
+                                    <th style="width: 1%;"><i class="far fa-check"></i></th>
+                                    <th style="width: 1%;"><i class="far fa-times"></i></th>
+                                    <th style="width: 1%;">Төлөв</th>
+                                    <th style="width: 15%;">Үйлдэл /засвартай/</th>
                                 </tr>
                                 </thead>
                                 <tbody>
+                                @foreach($audios as $audio)
                                 <tr>
-                                    <td scope="row">1</td>
+                                    <td scope="row">{{ $audio->id }}</td>
                                     <td>
-                                        <audio controls>
-                                            <source src="{{ asset('audio_files/agent-9009-1468979090-74251.wav') }}" type="audio_files/mp3">
-                                            <!--<source src="/path/to/audio.ogg" type="audio_files/ogg">-->
-                                        </audio>
+                                        <audio controls src="@if($audio->isLocal){{ asset($audio->url .  $audio->file) }}@else{{ $audio->url . $audio->file }}@endif" type="audio_files/wav"></audio>
                                     </td>
-                                    <td>Domuss sunt fluctuis de altus brodium.</td>
-                                    <td><div class="badge badge-danger">Зөвшөөрөөгүй</div></td>
-                                    <td>
-                                        <button type="button" class="btn btn-warning btn-sm" data-toggle="tooltip" data-placement="top" title="Засах"><i class="far fa-edit"></i></button>
-                                        <button type="button" class="btn btn-success btn-sm" data-toggle="tooltip" data-placement="top" title="Зөвшөөрөх"><i class="far fa-check"></i></button>
-                                        <button type="button" class="btn btn-danger btn-sm disabled" data-toggle="tooltip" data-placement="top" title="Зөвшөөрөхгүй"><i class="far fa-times"></i></button>
-                                        <button type="button" class="btn btn-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Устгах"><i class="far fa-trash"></i></button>
+                                    <td>@if($audio->tasks[0]->getLatestTranscribed() != null)
+                                            {{ $audio->tasks[0]->getLatestTranscribed()->transcription }}
+                                        @else
+                                            <i class="text-muted">Бичвэр ороогүй</i>
+                                        @endif
                                     </td>
-                                </tr>
-                                <tr>
-                                    <td scope="row">2</td>
                                     <td>
-                                        <audio controls>
-                                            <source src="{{ asset('audio_files/agent-9009-1468979090-74251.wav') }}" type="audio_files/mp3">
-                                            <!--<source src="/path/to/audio.ogg" type="audio_files/ogg">-->
-                                        </audio>
+                                        @if($audio->tasks[0]->getLatestTranscribed() != null)
+                                            {{ $audio->tasks[0]->getLatestTranscribed()->getRequiredValidation() }}
+                                        @else
+                                            <i class="text-muted">-</i>
+                                        @endif
                                     </td>
-                                    <td>Ratione de festus victrix, magicae nix!</td>
-                                    <td><div class="badge badge-success">Зөвшөөрсөн</div></td>
                                     <td>
-                                        <button type="button" class="btn btn-warning btn-sm" data-toggle="tooltip" data-placement="top" title="Засах"><i class="far fa-edit"></i></button>
-                                        <button type="button" class="btn btn-success btn-sm disabled" data-toggle="tooltip" data-placement="top" title="Зөвшөөрөх"><i class="far fa-check"></i></button>
-                                        <button type="button" class="btn btn-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Зөвшөөрөхгүй"><i class="far fa-times"></i></button>
-                                        <button type="button" class="btn btn-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Устгах"><i class="far fa-trash"></i></button>
+                                        @if($audio->tasks[0]->getLatestTranscribed() != null)
+                                            {{ $audio->tasks[0]->getLatestTranscribed()->getNumberOfAccepted() }}
+                                        @else
+                                            <i class="text-muted">-</i>
+                                        @endif
                                     </td>
-                                </tr>
-                                <tr>
-                                    <td scope="row">2</td>
                                     <td>
-                                        <audio controls>
-                                            <source src="{{ asset('audio_files/agent-9009-1468979090-74251.wav') }}" type="audio_files/mp3">
-                                            <!--<source src="/path/to/audio.ogg" type="audio_files/ogg">-->
-                                        </audio>
+                                        @if($audio->tasks[0]->getLatestTranscribed() != null)
+                                            {{ $audio->tasks[0]->getLatestTranscribed()->getNumberOfDeclined() }}
+                                        @else
+                                            <i class="text-muted">-</i>
+                                        @endif
                                     </td>
-                                    <td>Ratione de festus victrix, magicae nix!</td>
-                                    <td><div class="badge badge-warning">Зөвшөөрөл хүлээж байна</div></td>
                                     <td>
-                                        <button type="button" class="btn btn-warning btn-sm" data-toggle="tooltip" data-placement="top" title="Засах"><i class="far fa-edit"></i></button>
-                                        <button type="button" class="btn btn-success btn-sm" data-toggle="tooltip" data-placement="top" title="Зөвшөөрөх"><i class="far fa-check"></i></button>
-                                        <button type="button" class="btn btn-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Зөвшөөрөхгүй"><i class="far fa-times"></i></button>
-                                        <button type="button" class="btn btn-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Устгах"><i class="far fa-trash"></i></button>
+                                        @if($audio->tasks[0]->getLatestTranscribed() != null)
+                                            @if($audio->tasks[0]->getLatestTranscribed()->getRequiredValidation() == 0)
+                                                @if($audio->tasks[0]->getLatestTranscribed()->getValidationStatus() > 0)
+                                                    <div class="badge badge-success">Зөв</div>
+                                                @else
+                                                    <div class="badge badge-danger">Буруу</div>
+                                                @endif
+                                            @else
+                                                <div class="badge badge-warning">Шалгагдаж байна</div>
+                                            @endif
+                                        @else
+                                            <div class="badge badge-secondary">Бичвэр ороогүй</div>
+                                        @endif
                                     </td>
-                                </tr>
-                                <tr>
-                                    <td scope="row">2</td>
                                     <td>
-                                        <audio controls>
-                                            <source src="{{ asset('audio_files/agent-9009-1468979090-74251.wav') }}" type="audio_files/mp3">
-                                            <!--<source src="/path/to/audio.ogg" type="audio_files/ogg">-->
-                                        </audio>
-                                    </td>
-                                    <td></td>
-                                    <td><div class="badge badge-secondary">Бичвэргүй</div></td>
-                                    <td>
-                                        <button type="button" class="btn btn-warning btn-sm" data-toggle="tooltip" data-placement="top" title="Засах"><i class="far fa-edit"></i></button>
+                                        <button type="button" class="btn btn-warning btn-sm disabled" data-toggle="tooltip" data-placement="top" title="Засах"><i class="far fa-edit"></i></button>
                                         <button type="button" class="btn btn-success btn-sm disabled" data-toggle="tooltip" data-placement="top" title="Зөвшөөрөх"><i class="far fa-check"></i></button>
                                         <button type="button" class="btn btn-danger btn-sm disabled" data-toggle="tooltip" data-placement="top" title="Зөвшөөрөхгүй"><i class="far fa-times"></i></button>
-                                        <button type="button" class="btn btn-danger btn-sm" data-toggle="tooltip" data-placement="top" title="Устгах"><i class="far fa-trash"></i></button>
+                                        <button type="button" class="btn btn-danger btn-sm disabled" data-toggle="tooltip" data-placement="top" title="Устгах"><i class="far fa-trash"></i></button>
                                     </td>
                                 </tr>
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
+                        <div aria-label="Page navigation" class="row justify-content-center">
+                            <ul class="pagination">
+                                <li class="page-item @if($page == 1)disabled @endif">
+                                    <a class="page-link" href="{{ $request->fullUrlWithQuery(['page' => 1]) }}" aria-label="First">
+                                        <span aria-hidden="true"><i class="far fa-chevron-double-left"></i></span>
+                                        <span class="sr-only">Эхнийх</span>
+                                    </a>
+                                </li>
+                                <li class="page-item @if($page == 1)disabled @endif">
+                                    <a class="page-link" href="{{ $request->fullUrlWithQuery(['page' => $page - 1]) }}" aria-label="Previous">
+                                        <span aria-hidden="true"><i class="far fa-chevron-left"></i></span>
+                                        <span class="sr-only">Өмнөх</span>
+                                    </a>
+                                </li>
+                                @for($i=1; $i<=$total_page; $i++)
+                                    <li class="page-item @if($page == $i)active @endif"><a class="page-link" href="{{ $request->fullUrlWithQuery(['page' => $i]) }}">{{ $i }}</a></li>
+                                @endfor
+                                <li class="page-item @if($page == $total_page)disabled @endif">
+                                    <a class="page-link" href="{{ $request->fullUrlWithQuery(['page' => $page + 1]) }}" aria-label="Next">
+                                        <span aria-hidden="true"><i class="far fa-chevron-right"></i></span>
+                                        <span class="sr-only">Дараах</span>
+                                    </a>
+                                </li>
+                                <li class="page-item @if($page == $total_page)disabled @endif">
+                                    <a class="page-link" href="{{ $request->fullUrlWithQuery(['page' => $total_page]) }}" aria-label="Last">
+                                        <span aria-hidden="true"><i class="far fa-chevron-double-right"></i></span>
+                                        <span class="sr-only">Сүүлийнх</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <!--</div>-->
-                    <!--</div>-->
-                    <!--<div class="col-lg-4 col-md-4 col-sm-10">-->
-                    <!---->
-                    <!--</div>-->
                 </div>
             </div>
         </div>
@@ -119,32 +156,9 @@
 @section('scripts')
     @parent
     <script src="{{ asset('assets/js/plyr.js') }}"></script>
-    {{--<script src="{{ asset('assets/js/jquery.dataTables.min.js') }}"></script>--}}
-    {{--<script src="{{ asset('assets/js/dataTables.bootstrap4.min.js') }}"></script>--}}
     <script>
         $(document).ready(function() {
             plyr.setup();
-//            var table = $('#audio-table').DataTable({
-//                language: {
-//                    oPaginate: {
-//                        sNext: '<i class="far fa-arrow-right"></i>',
-//                        sPrevious: '<i class="far fa-arrow-left"></i>',
-//                        sFirst: '<i class="far fa-arrow-to-left"></i>',
-//                        sLast: '<i class="far fa-arrow-to-right"></i>'
-//                    }
-//
-//                } ,
-//                pagingType: 'full_numbers'
-//            });
-//            $('[data-toggle="tooltip"]').tooltip();
-//            plyr.setup();
-//
-//            plyr.on('destroyed', function () {
-//
-//            });
-//            table.on('draw', function () {
-//                plyr.destroy();
-//            })
         });
     </script>
 @endsection
