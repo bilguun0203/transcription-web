be7fb1f832d2d41809cd638d2a27c01103f386e6
Bilguun Ochirbat <bilguun0203@gmail.com>
2018-01-07 15:40:58 +0800

    Хэрэглэгчийн төлөв

--- app/User.php
+++ app/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->isAdmin;
     }
+
+    public function isBanned()
+    {
+        return $this->status == 0 ? true : false;
+    }
 }
