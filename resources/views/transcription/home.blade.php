b771c00408e824e42b409b12eeb0d4e778b3e44a
Bilguun Ochirbat <bilguun0203@gmail.com>
2018-04-07 16:06:06 +0800

    Нүүр хуудасны элементүүдийн байрлал болон тайлбаруудыг өөрчилсөн

--- resources/views/transcription/home.blade.php
+++ resources/views/transcription/home.blade.php
@@ -38,67 +38,77 @@
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-8">
                 <div class="card">
                     <div class="card-body">
-                        <p>Аудио файл дахь яриаг бичвэр болгох.</p>
+                        <p>Аудио файл дахь яриаг заасан дүрмийн дагуу бичвэр болгох.</p>
                     </div>
                     <div class="card-footer">
                         <a href="{{ route('transcribe') }}" class="btn btn-info btn-block btn-raised"><i class="far fa-pencil"></i> Бичвэрт буулгах</a>
                     </div>
                 </div>
             </div>
-            @auth
-            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-8">
-                <div class="card">
-                    <div class="card-header">
-                        Тоон үзүүлэлт
-                    </div>
-                    <div class="card-body">
-                        <h6>Миний бичвэр болгосон</h6>
-                        <ul>
-                            <li>Нийт: {{ $status['transcribe']['a'] + $status['transcribe']['d'] + $status['transcribe']['p'] }}</li>
-                            <li>Зөвшөөрөгдсөн: {{ $status['transcribe']['a'] }}</li>
-                            <li>Зөвшөөрөөгүй: {{ $status['transcribe']['d'] }}</li>
-                            <li>Хүлээгдэж байгаа: {{ $status['transcribe']['p'] }}</li>
-                            <li><strong>Оноо:</strong> {{ $score['transcribe'] }}</li>
-                        </ul>
-                        <h6>Миний шалгасан</h6>
-                        <ul>
-                            <li>Нийт: {{ $status['validate']['a'] + $status['validate']['d'] }}</li>
-                            <li>Зөв: {{ $status['validate']['a'] }}</li>
-                            <li>Буруу: {{ $status['validate']['d'] }}</li>
-                            <li><strong>Оноо:</strong> {{ $score['validate'] }}</li>
-                        </ul>
-                        <h6>Миний зассан</h6>
-                        <ul>
-                            <li>Нийт: {{ $status['edit'] }}</li>
-                            <li><strong>Оноо:</strong> {{ $score['edit'] }}</li>
-                        </ul>
-                        <h6>Нийт оноо: {{ $score['transcribe'] + $score['validate'] + $score['edit'] }}</h6>
-                    </div>
-                </div>
-            </div>
-            @endauth
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-8">
                 <div class="card">
                     <div class="card-body">
-                        <p>Бичвэр болгосон яриаг сонсож таарч байгаа эсэхийг шалгах.</p>
+                        <p>Бичвэр болгосон яриаг сонсож таарч байгаа эсэхийг шалгаж "зөв" болон "буруу" гэсэн саналын аль нэгийг өгнө.</p>
                     </div>
                     <div class="card-footer">
                         <a href="{{ route('validate') }}" class="btn btn-danger btn-block btn-raised"><i class="far fa-check"></i> Шалгах</a>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row justify-content-center mt-5">
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-8">
                 <div class="card">
                     <div class="card-body">
-                        <p>Аудио файлын бичвэрийг засах.</p>
+                        <p>Аудио файлд бичвэр оруулахад алдаа гаргасан эсэхийг шалгаж гарсан бол түүнийг нь дүрмийн дагуу засаад хадгална.</p>
                     </div>
                     <div class="card-footer">
                         <a href="{{ route('edit_transcription') }}" class="btn btn-primary btn-block btn-raised"><i class="far fa-pencil"></i> Бичвэр засах</a>
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="row justify-content-center mt-3">
+            @auth
+                <div class="col-lg-9 col-md-9 col-sm-10 col-xs-12">
+                    <div class="card">
+                        <div class="card-header">
+                            Тоон үзүүлэлт
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                    <h6>Миний бичвэр болгосон</h6>
+                                    <ul>
+                                        <li>Нийт: {{ $status['transcribe']['a'] + $status['transcribe']['d'] + $status['transcribe']['p'] }}</li>
+                                        <li>Зөвшөөрөгдсөн: {{ $status['transcribe']['a'] }}</li>
+                                        <li>Зөвшөөрөөгүй: {{ $status['transcribe']['d'] }}</li>
+                                        <li>Хүлээгдэж байгаа: {{ $status['transcribe']['p'] }}</li>
+                                        <li><strong>Оноо:</strong> {{ $score['transcribe'] }}</li>
+                                    </ul>
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                    <h6>Миний шалгасан</h6>
+                                    <ul>
+                                        <li>Нийт: {{ $status['validate']['a'] + $status['validate']['d'] }}</li>
+                                        <li>Зөв: {{ $status['validate']['a'] }}</li>
+                                        <li>Буруу: {{ $status['validate']['d'] }}</li>
+                                        <li><strong>Оноо:</strong> {{ $score['validate'] }}</li>
+                                    </ul>
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                    <h6>Миний зассан</h6>
+                                    <ul>
+                                        <li>Нийт: {{ $status['edit'] }}</li>
+                                        <li><strong>Оноо:</strong> {{ $score['edit'] }}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <h5>Нийт оноо: {{ $score['transcribe'] + $score['validate'] + $score['edit'] }}</h5>
+                        </div>
+                    </div>
+                </div>
+            @endauth
         </div>
         @auth
             @if(Auth::user()->isAdmin())
